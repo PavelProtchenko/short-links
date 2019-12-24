d02d47afe03baec2325a3d6b97b1221c0cdaf99a
PavelProtchenko <protchenkopa@gmail.com>
2019-12-24 23:32:19 +0300

    add connection to mongodb

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express')
-const app = express()
 const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
 
 const PORT = config.get('port') || 5000
 
-app.listen(PORT, () => console.log(`Short links has been started on port ${PORT}`))
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+    app.listen(PORT, () => console.log(`Short links has been started on port ${PORT}`))
+  } catch(e) {
+    console.log('Server error', e.message)
+    process.exit(1)
+  }
+}
+
+start()
