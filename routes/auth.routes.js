c76ec621312c3e7012624ebcb4df8c05861dc94d
PavelProtchenko <protchenkopa@gmail.com>
2019-12-25 00:51:47 +0300

    add logics to user login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -41,8 +41,40 @@ router.post('/register',
   }
 })
 
-router.post('/login', async (req, res) => {
+router.post('/login',
+  [
+    check('email', 'Enter valid email').normalizeEmail().isEmail(),
+    check('password', 'Enter your password').exists()
+  ],
+  async (req, res) => {
+  try {
+    const errors = validationResult(req)
 
+    if (!errors.isEmpty) {
+      return res.status(400).json({
+        errors: errors.array(),
+        message: 'Invalid login data'
+      })
+    }
+
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      res.status(400).json({ message: 'User not found' })
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid password, try again' })
+    }
+
+
+
+  } catch (e) {
+    res.status(400).json({ message: 'Something went wrong, try again later' })
+  }
 })
 
 module.exports = router
