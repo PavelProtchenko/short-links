3b32e7549003b13fd6119256bdb2611e590cc47f
PavelProtchenko <protchenkopa@gmail.com>
2019-12-24 22:34:38 +0300

    add config and create server for backend

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Hello Shot links')
+const express = require('express')
+const app = express()
+const config = require('config')
+
+const PORT = config.get('port') || 5000
+
+app.listen(PORT, () => console.log(`Short links has been started on port ${PORT}`))
